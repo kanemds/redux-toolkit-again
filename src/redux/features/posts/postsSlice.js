c37e242726cc02a156f14f33a791ac19e824f120
Kanem <kanemdeng@email.com>
2022-10-26 15:07:44 -0700

    created a single post page

--- src/redux/features/posts/postsSlice.js
+++ src/redux/features/posts/postsSlice.js
@@ -114,6 +114,8 @@ const postsSlice = createSlice({
 export const selectAllPosts = (state) => state.posts.posts
 export const getPostsStatus = (state) => state.posts.status
 export const getPostsError = (state) => state.posts.error
+export const getSinglePost = (state, postId) => state.posts.posts.find(post => post.id === postId)
+
 
 export const { addedNewPost, addedReaction } = postsSlice.actions
 export default postsSlice.reducer
