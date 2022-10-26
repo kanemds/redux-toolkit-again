1dd343bc005119fe2eb0dfd5d8f49484bdd0b8cd
Kanem <kanemdeng@email.com>
2022-10-26 15:31:16 -0700

    setting up some routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css'
+import Layout from './components/Layout'
 import NewPostForm from './redux/features/posts/NewPostForm'
 import PostsList from './redux/features/posts/PostsList'
-
+import { Routes, Route } from 'react-router-dom'
+import SinglePost from './redux/features/posts/SinglePost'
 
 function App() {
   return (
-    <div className="App">
-      <NewPostForm />
-      <PostsList />
-    </div>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+
+        <Route index element={<PostsList />} />
+
+        <Route path='post'>
+          <Route index element={<NewPostForm />} />
+          <Route path=':postId' element={<SinglePost />} />
+        </Route>
+
+      </Route>
+    </Routes>
   )
 }
 
